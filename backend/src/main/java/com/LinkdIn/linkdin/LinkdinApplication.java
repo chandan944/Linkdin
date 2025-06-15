80663995d2bdcb8622e8a4e8f6e7c8c4ac6b1d49
chandan prajapati <chandanprajapati6307@>
2025-06-15 07:33:25 +0530

    chandan : setup frantend and backend . here we go

---
+++ backend/src/main/java/com/LinkdIn/linkdin/LinkdinApplication.java
@@ -0,0 +1,13 @@
+package com.LinkdIn.linkdin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkdinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkdinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/LinkdIn/linkdin/LinkdinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LinkdIn.linkdin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkdinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
